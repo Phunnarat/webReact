618723bb83d167aa6566090bb897e4e9a98b2d40
Phunnarat <s6203051613191@email.kmutnb.ac.th>
2022-08-26 11:28:41 +0700

    add class-props.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import {Header, Content, Footer} from './func-components'
+import MsgBox from './class-props'
 
 function App() {
   return (
-   [<Header/>, <Content/>, <Footer/>]
+    <MsgBox
+      text="React & React Native"
+      color = "blue"
+      bgColor= "#ccc"
+      fontSize = "18pt"
+      border = "solid 1px black"
+    />
   )
 }
 
